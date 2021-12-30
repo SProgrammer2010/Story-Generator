6df0387a289d76939bfe6d9296f9b1ddef66d0ee
SProgrammer2010 <95369436+SProgrammer2010@users.noreply.github.com>
2021-12-30 23:22:18 +0400

    Add files via upload

---
+++ story generator.py
@@ -0,0 +1,10 @@
+print("This Python Project will generate a story based on the inputs of the user")
+
+Location = input("Location: ")
+Object = input("An object at the location: ")
+Tool = input("A Tool near the object: ")
+Char = input("Character: ")
+
+print(Char + " was having fun at the " + Location + " with his friends as he was having fun, an evil monster appeared over there. " + Char + " ran to the " + Object)
+print("and grabbed a " + Tool + " with it he tried to defend himself and drove away the monster with his bravery his name was written in the book of justice")
+print("and displayed on the " + Location + "s display board. The story has ended.")
